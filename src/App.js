b89b4acb239b2a0c843b0951176ba29fb9b20a9b
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-29 17:14:06 +0200

    Adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, I'm Quynh Anh. I'm a fullstack web developer.
-        </p>
+      <header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="wrapper text-lg">
+          <h1 className="text-lg">Hi, I'm Quynh Anh !</h1>
+          <h1 className="text-lg">I'm a <strong className="text-lg">fullstack web developer</strong></h1>
+          <span className="container-flip text-lg">
+            <span className="flip text-lg">and coffee addict ☕</span>
+            <span className="flip text-lg">and cat lover 🐾</span>
+            <span className="flip text-lg">and choir singer 🎙</span>
+            <span className="flip text-lg">and sewista 🧵</span>
+            <span className="flip text-lg">and spicy food eater 🌶</span>
+          </span>
+        </div>
       </header>
       <main>
         {/* <Navbar />
