340bc7adc2182c03186e7cd4d9312ebaaf3ab4d8
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-30 16:51:05 +0200

    Adding a profile pic and adding some tailwind css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import About from './components/About';
 function App() {
   return (
     <div className="App">
-      <header className="text-green-900 bg-pink-200">
-        <div className="wrapper text-lg">
-          <h1 className="text-lg">Hi, I'm Quynh Anh !</h1>
-          <h1 className="text-lg">I'm a <strong className="text-lg">fullstack web developer</strong></h1>
-          <span className="container-flip text-lg">
-            <span className="flip text-lg">and coffee addict ☕</span>
-            <span className="flip text-lg">and cat lover 🐾</span>
-            <span className="flip text-lg">and choir singer 🎙</span>
-            <span className="flip text-lg">and sewista 🧵</span>
-            <span className="flip text-lg">and spicy food eater 🌶</span>
+      <header className="text-green-900 bg-pink-200 h-52 md:h-96">
+        <div className="wrapper pt-10 px-10 lg:pt-20 lg:pb-10 lg:px-20">
+          <h1 className="text-base md:text-lg lg:text-3xl">Hi, I'm Quynh Anh !</h1>
+          <h1 className="text-base md:text-lg lg:text-3xl">I'm a <strong className="text-base md:text-lg lg:text-3xl">fullstack web developer</strong></h1>
+          <span className="container-flip h-8 lg:h-14">
+            <span className="flip text-base md:text-lg lg:text-3xl">and coffee addict ☕</span>
+            <span className="flip text-base md:text-lg lg:text-3xl">and cat lover 🐾</span>
+            <span className="flip text-base md:text-lg lg:text-3xl">and choir singer 🎙</span>
+            <span className="flip text-base md:text-lg lg:text-3xl">and sewista 🧵</span>
+            <span className="flip text-base md:text-lg lg:text-3xl">and spicy food eater 🌶</span>
           </span>
         </div>
       </header>
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import profilepic from '../assets/profilepic.jpg';
 
 const About = () => {
   return (
-  <section id="about">
+  <section id="about" className="p-10 text-center md:p-20 md:text-left">
     <h2 className="text-lg p-2">About</h2>
-    <p className="text-sm p-2">Former communication officer in a concert venue, I have just completed a fullstack web development course at le Wagon Brussels.
-Skilled in communication and customer oriented, I am looking forward to learn more programming languages and build new apps.</p>
+    <div className="p-10 md:p-20 md:flex">
+      <img src={profilepic} alt="profilepic" className="m-1 w-1/2 h-1/2 md:w-1/4 md:1/4 rounded-full"/>
+      <p className="p-2 text-xs sm:text-base md:text-lg text-center">Former communication officer in a concert venue, I have just completed a fullstack web development course at le Wagon Brussels.
+  Skilled in communication and customer oriented, I am looking forward to learn more programming languages and build new apps.</p>
+    </div>
     <a
       href="#contact"
-      className="inline-flex text-xs text-white bg-green-900 border-0 py-2 px-4 focus:outline-none hover:bg-pink-200 hover:text-green-900
+      className="inline-flex sm:text-base md:text-lg text-white bg-green-900 border-0 py-2 px-4 focus:outline-none hover:bg-pink-200 hover:text-green-900
 
        rounded">
       Let's have a chat!
