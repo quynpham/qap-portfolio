9887b54afd7ef2e9712a04066ff111f49b5a5be9
Quynh Anh Pham <qap.pham@gmail.com>
2022-04-01 12:30:59 +0200

    Refactoring the projects into a data file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,50 +1,33 @@
 import React from "react";
-import recyclaidpic from "../assets/recyclaidpic.png";
-import topletspic from "../assets/topletspic.png";
+import ProjectsData from "./ProjectsData";
 
 const Projects = () => {
   return (
     <section id="projects" className="w-screen mx-auto p-5 sm:p-10 md:p-16 bg-pistachio text-framboise">
       <h2>Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-        <div className="max-w-md rounded overflow-hidden shadow-lg">
-          <a href="http://www.recyclaid.xyz/" target="_blank" rel="noreferrer">
-            <div className="relative">
-              <img className="w-full" src={recyclaidpic} alt="homepage recyclaid"/>
-              <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
-            </div>
-          </a>
-          <a href="http://www.recyclaid.xyz/" target="_blank" rel="noreferrer">
-            <div className="min-h-full px-6 py-4 bg-white">
-                <h3 className="font-bold">RECYCLAID</h3>
-                <span className="text-sm">Recyclaid is a platform where users can sell or buy cheaper construction materials in a sustainable way, contributing to the circular economy.
-                  <br></br>
-                  <em>Created by Kato de Lentacker, Dorian Della Faille and I during the bootcamp we followed at Le Wagon Brussels.</em>
-                </span>
-            </div>
-          </a>
+      {ProjectsData.map((Project) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="max-w-md rounded overflow-hidden shadow-lg">
+            <a href={Project.link} target="_blank" rel="noreferrer">
+              <div className="relative">
+                <img className="w-full" src={Project.image} alt={Project.title}/>
+                <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+              </div>
+            </a>
+            <a href={Project.link} target="_blank" rel="noreferrer">
+              <div className="min-h-full px-6 py-4 bg-white">
+                  <h3 className="font-bold">{Project.title}</h3>
+                  <span className="text-sm">{Project.description}
+                    <em>{Project.credits}</em>
+                  </span>
+              </div>
+            </a>
+          </div>
         </div>
-
-        <div className="max-w-md rounded overflow-hidden shadow-lg">
-          <a href="https://toilets-airbnb.herokuapp.com/" target="_blank" rel="noreferrer">
-            <div className="relative">
-              <img className="w-full" src={topletspic} alt="homepage Toplets"/>
-              <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
-            </div>
-          </a>
-          <a href="https://toilets-airbnb.herokuapp.com/" target="_blank" rel="noreferrer">
-            <div className="min-h-full px-6 py-4 bg-white">
-                <h3 className="font-bold">TOPLETS</h3>
-                <span className="text-sm">AirBnb clone but to rent toilets.
-                  <br></br>
-                  <em>Created by Kato de Lentacker, Dorian Della Faille and I during the bootcamp we followed at Le Wagon Brussels.</em>
-                </span>
-            </div>
-          </a>
-        </div>
-      </div>
+      ))
+      }
     </section>
-)
+  )
 }
 
 export default Projects;
---
+++ src/components/ProjectsData.js
@@ -0,0 +1,22 @@
+import recyclaidpic from "../assets/recyclaidpic.png";
+import topletspic from "../assets/topletspic.png";
+
+const projects = [
+  {
+    title: "RECYCLAID",
+    description: "Recyclaid is a platform where users can sell or buy cheaper construction materials in a sustainable way, contributing to the circular economy.",
+    credits: "Created by Kato de Lentacker, Dorian Della Faille and I during the bootcamp we followed at Le Wagon Brussels.",
+    image:`${recyclaidpic}`,
+    link: "http://www.recyclaid.xyz/",
+  },
+  {
+    title: "TOPLETS",
+    description: "AirBnb clone but to rent toilets.",
+    credits: "Created by Kato de Lentacker, Dorian Della Faille and I during the bootcamp we followed at Le Wagon Brussels.",
+    image: `${topletspic}`,
+    link: "https://toilets-airbnb.herokuapp.com/",
+  },
+
+]
+
+export default projects;
