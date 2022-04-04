58214cbf0d65f65b63efa99830c545d3a2e010c4
Quynh Anh Pham <qap.pham@gmail.com>
2022-04-04 16:14:39 +0200

    Adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -6,17 +8,21 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-      </header>
-      <main>
-        {/* <Navbar /> */}
-        <Home/>
-        <About/>
-        <Projects />
-        <Contact />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/About' exact component={About} />
+            <Route path='/Projects' exact component={Projects} />
+            <Route path='/Contact' exact component={Contact} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
