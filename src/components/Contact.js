36b8eee5b08d175d39422afcd42aca3bea0d54c1
Quynh Anh Pham <qap.pham@gmail.com>
2022-04-04 15:52:34 +0200

    Finishing the form with a hidden html form and useState to store data

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,37 @@
 import React from "react";
 
 const Contact = () => {
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
     <section id="contact" className="relative text-seeweed ">
       <div className="py-10 mx-auto flex sm:flex-nowrap flex-wrap md:px-20 md:text-left px-5">
         <form
           netlify
           name="contact"
+          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:m-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-center text-seeweed text-5xl p-2">
             Contact
@@ -23,6 +48,7 @@ const Contact = () => {
               id="name"
               name="name"
               className="w-full bg-transparent rounded border border-gray-300 focus:border-seeweed focus:ring-2 focus:ring-seeweed text-base outline-none text-seeweed py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -34,6 +60,7 @@ const Contact = () => {
               id="email"
               name="email"
               className="w-full bg-transparent rounded border border-gray-300 focus:border-seeweed focus:ring-2 focus:ring-seeweed text-base outline-none text-seeweed py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -46,6 +73,7 @@ const Contact = () => {
               id="message"
               name="message"
               className="w-full bg-transparent rounded border border-gray-300 focus:border-seeweed focus:ring-2 focus:ring-seeweed text-base outline-none text-seeweed py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
