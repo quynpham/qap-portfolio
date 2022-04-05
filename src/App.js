78d7e6d0e1b654bb7b8673955bb9cacdf1369922
Quynh Anh Pham <qap.pham@gmail.com>
2022-04-05 17:45:17 +0200

    Styling and toggle of the navbar menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,21 +7,17 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <header>
           <Navbar />
         </header>
         <main>
-          <Routes>
-            <Route path='/' exact component={Home} />
-            <Route path='/About' exact component={About} />
-            <Route path='/Projects' exact component={Projects} />
-            <Route path='/Contact' exact component={Contact} />
-          </Routes>
+          <Home/>
+          <About/>
+          <Projects/>
+          <Contact/>
         </main>
       </div>
-    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen ] = useState(false)
+
+  const handleToggle = () => setNavbarOpen(!navbarOpen)
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
+
   return(
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/About">About</Link>
-        </li>
-        <li>
-          <Link to="/Projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="text-seeweed bg-transparent text-3xl">
+      <div className="flex justify-between relative z-40 wrapper py-4">
+        <a href="/" className="">Home</a>
+        <button onClick={handleToggle} className="">{ navbarOpen ? "Close": "Open" }</button>
+      </div>
+      <div className="flex flex-col wrapper">
+        <ul className={`overflow-hidden fixed top-0 left-0 bottom-0 h-screen z-10 ${navbarOpen ? "showMenu w-full" : "w-0"}`}>
+          <li className="text-3xl text-center py-4 mb-4 border-b border-gray-50">
+            <a href="#about"
+            className="px-1"
+            onClick={() => closeMenu()}
+            >
+            About</a>
+          </li>
+          <li className="text-3xl text-center py-4 mb-4 border-b border-gray-50">
+            <a href="#projects" className="px-1"
+            onClick={() => closeMenu()}>Projects</a>
+          </li>
+          <li className="text-3xl text-center py-4 mb-4 border-b border-gray-50">
+            <a href="#contact" className="px-1"
+            onClick={() => closeMenu()}>Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
